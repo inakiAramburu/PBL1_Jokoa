13c72e54cfbeedda33a1e3841e854c9e183c4edb
inakiAramburu <inaki.aramburur@alumni.mondragon.edu>
2019-12-18 15:17:40 +0100

    como dev

--- TheCave/Jokoa/Funtzioak.c
+++ TheCave/Jokoa/Funtzioak.c
@@ -5,7 +5,7 @@ SDL_Window* window;
 
 void LeihoaHasi()
 {
-            // Initialize SDL2
+	// Initialize SDL2
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Ezin izan da SDL hasieratu: %s", SDL_GetError());
 		return 1;
@@ -29,63 +29,6 @@ void LeihoaHasi()
 
 //Fondoa jartzen du.
 void AtzekoPlanoBerria(char AtzekoPlanoa[])
-{
-
-	SDL_Renderer* renderer;
-	SDL_Surface* surface;
-	SDL_Texture* texture;
-	SDL_Event event;
-
-
-	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
-	//irudia kargatu
-	surface = SDL_LoadBMP(AtzekoPlanoa);
-	if (!surface) {
-		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Ezin da argazkitik azalera sortu: %s\n", SDL_GetError());
-		return 1;
-	}
-	texture = SDL_CreateTextureFromSurface(renderer, surface);
-	if (!texture) {
-		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Ezin da azaleratik textura sortu: %s\n", SDL_GetError());
-		return 1;
-	}
-
-	SDL_FreeSurface(surface);		//Aurrekoa garbitzeko
-
-	while (1) {
-		SDL_PollEvent(&event);
-		if (event.type == SDL_QUIT) {
-			break;
-		}
-		SDL_RenderClear(renderer);
-		SDL_RenderCopy(renderer, texture, NULL, NULL);
-		SDL_RenderPresent(renderer);
-		variable();
-
-	}
-
-	return 0;
-
-}
-
-
-
-void variable()
-{
-
-	SDL_Surface* jugador;
-	SDL_Rect posicion;
-
-	jugador = SDL_LoadBMP("Nivel5.bmp");
-	posicion.x = 320;
-	posicion.y = 400;
-
-	SDL_BlitSurface(jugador, NULL, window, &posicion);
-}
-
-
-void AtzekoPlanoBerria2(char AtzekoPlanoa[])
 {
 
 	SDL_Renderer* renderer;
