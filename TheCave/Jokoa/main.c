f01b347978e50f5771962cf1b4f75fb26f407de7
inakiAramburu <inaki.aramburur@alumni.mondragon.edu>
2019-12-18 15:18:31 +0100

    como dev

--- TheCave/Jokoa/main.c
+++ TheCave/Jokoa/main.c
@@ -4,13 +4,12 @@
 
 int main(int argc, char* str[]) {
 	char Atzekoplanoa[256] = ".\\img\\Menu.bmp";		//Argazkiaren helbidea
-	char Atzekoplanoa2[256] = ".\\img\\Menu.bmp";
 	LeihoaHasi();
 	
 	SDL_Delay(2000);		//Txuriz 2 segundu
 
 	AtzekoPlanoBerria(Atzekoplanoa);
-	AtzekoPlanoBerria2(Atzekoplanoa2);
+
 	
 
 
