71a83b14e7671850d9d4988bc26931fa050d5e37
Mikel Uribesalgo Arbulu <mikel.uribesalgo@alumni.mondragon.edu>
2019-12-18 14:36:24 +0100

    rama mikel 4

--- TheCave/Jokoa/main.c
+++ TheCave/Jokoa/main.c
@@ -1,14 +1,33 @@
 #include "Funtzioak.h"
 #include <stdio.h>
-//h
+//Ekaitz
 
 int main(int argc, char* str[]) {
 	char Atzekoplanoa[256] = ".\\img\\Menu.bmp";		//Argazkiaren helbidea
-	LeihoaHasi();
-	
+	char Atzekoplanoa2[256] = ".\\img\\Nivel2.bmp";
+	if (LeihoaHasi() == 1)
+	{
+		printf("Ezin izan da 1280x720- ko bideoa ezarri: %s\n", SDL_GetError());
+		return 1;
+	}
+
 	SDL_Delay(2000);		//Txuriz 2 segundu
 
-	AtzekoPlanoBerria(Atzekoplanoa);
-	
+
+	if (AtzekoPlanoBerria(Atzekoplanoa) == 1)
+	{
+		fprintf(stderr, "Ezin izan da atzeko palnoa kargatu: %s\n", SDL_GetError());
+		return 1;
+	}
+	SDL_Delay(2000);		//Txuriz 2 segundu
+	if (AtzekoPlanoBerria(Atzekoplanoa2) == 1)
+	{
+		fprintf(stderr, "Eziin izan da atzeko palnoa kargatu: %s\n", SDL_GetError());
+		return 1;
+	}
+	SDL_Delay(2000);		//Txuriz 2 segundu
 	return 0;
 }
+
+
+
