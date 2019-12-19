f938b79ec4a01fd322d9131c117b0d7abc669bcf
9ekaitz <ekaitz.casado@alumni.mondragon.edu>
2019-12-19 08:55:12 +0100

    Konponduta

--- TheCave/Jokoa/main.c
+++ TheCave/Jokoa/main.c
@@ -3,20 +3,19 @@
 //dev
 
 int main(int argc, char* str[]) {
-	char Atzekoplanoa[256] = ".\\img\\Menu.bmp";		//Argazkiaren helbidea
+	char Atzekoplanoa[256] = ".\\img\\Menu.bmp", Atzekoplanoa2[128] = ".\\img\\Nivel2.bmp";		//Argazkiaren helbidea
 	if (LeihoaHasi() == 1)
 	{
 		printf("Ezin izan da 1280x720- ko bideoa ezarri: %s\n", SDL_GetError());
 		return 1;
 	}
 	
-	SDL_Delay(2000);		//Txuriz 2 segundu
 
 	if (AtzekoPlanoBerria(Atzekoplanoa) == 1)
 	{
 		fprintf(stderr, "Ezin izan da atzeko palnoa kargatu: %s\n", SDL_GetError());
 		return 1;
 	}
-	
+	SDL_Delay(2000);		//Txuriz 2 segundu
 	return 0;
 }
