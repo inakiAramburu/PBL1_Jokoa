316e6d296e4c1d305a71b53c83346f3a0463a759
inakiAramburu <inaki.aramburur@alumni.mondragon.edu>
2019-12-19 09:55:58 +0100

    carpetas

--- TheCave/Jokoa/Funtzioak.h
+++ TheCave/Jokoa/Funtzioak.h
@@ -4,7 +4,7 @@
 
 #define PANTAILA_ALTUERA 720
 #define PANTAILA_ZABALERA 1280
-
+//
 int LeihoaHasi();
 int Argazkia_Sartu(char AtzekoPlanoa[], int Posx, int Posy, int luzeera, int altuera);
 int AtzekoPlanoBerria(char AtzekoPlanoa[]);
