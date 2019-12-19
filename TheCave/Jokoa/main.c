45fd3e39d0ba4e2398fb5f71924941c14060a7ce
itxaso2019 <itxaso.soria@alumni.mondragon.edu>
2019-12-19 12:10:11 +0100

    player en el menu

--- TheCave/Jokoa/main.c
+++ TheCave/Jokoa/main.c
@@ -8,8 +8,10 @@ int main(int argc, char* str[]) {
 	char Jolastu[128] = ".\\media\\menu\\Jolastu.bmp";
 	char Kontrolak[128] = ".\\media\\menu\\Kontrolak.bmp";
 	char Kredituak[128] = ".\\media\\menu\\Kredituak.bmp";
+	char Pertsonaia[128] = ".\\media\\player\\Player\\run\\run1.bmp";
 	//char Musika2[128] = ".\\media\\sound\\test.wav";
 	//MusikaJarri(Musika2);
+	
 	LeihoaHasi();
 
 	SDL_Delay(2000);		//Txuriz 2 segundu
@@ -18,6 +20,7 @@ int main(int argc, char* str[]) {
 	Argazkia_Sartu(Jolastu, 500, 210, 270, 65);
 	Argazkia_Sartu(Kontrolak, 480, 300, 300, 72);
 	Argazkia_Sartu(Kredituak, 480, 400, 300, 72);
+	Argazkia_Sartu(Pertsonaia, -10, 532, 150, 100);
 
 	
 
