53c141c2ce9c9d8b2feaf10319947f3734a590cd
9ekaitz <ekaitz.casado@alumni.mondragon.edu>
2019-12-19 11:43:18 +0100

    Ordena aldatu

--- TheCave/Jokoa/Funtzioak.c
+++ TheCave/Jokoa/Funtzioak.c
@@ -29,6 +29,38 @@ int LeihoaHasi()
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 }
 
+//Fondoa jartzen du.
+int AtzekoPlanoBerria(char AtzekoPlanoa[])
+{
+
+	SDL_Surface* surface;
+	SDL_Texture* texture;
+	SDL_Event event;
+
+
+	//irudia kargatu
+	surface = SDL_LoadBMP(AtzekoPlanoa);
+	if (!surface) {
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Ezin da argazkitik azalera sortu: %s\n", SDL_GetError());
+		return 1;
+	}
+	texture = SDL_CreateTextureFromSurface(renderer, surface);
+	if (!texture) {
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Ezin da azaleratik textura sortu: %s\n", SDL_GetError());
+		return 1;
+	}
+
+	SDL_FreeSurface(surface);		//Aurrekoa garbitzeko
+	SDL_PollEvent(&event);
+	SDL_RenderClear(renderer);
+	SDL_RenderCopy(renderer, texture, NULL, NULL);
+	SDL_RenderPresent(renderer);
+
+
+	return 0;
+
+}
+
 //Argazkia jartzen du.
 int Argazkia_Sartu(char AtzekoPlanoa[], int Posx, int Posy, int luzeera, int altuera)
 {
@@ -61,38 +93,6 @@ int Argazkia_Sartu(char AtzekoPlanoa[], int Posx, int Posy, int luzeera, int alt
 	SDL_FreeSurface(surface);		//Aurrekoa garbitzeko
 	SDL_PollEvent(&event);
 	SDL_RenderCopy(renderer, texture, NULL, &neurriak);
-	SDL_RenderPresent(renderer);
-
-
-	return 0;
-
-}
-
-//Fondoa jartzen du.
-int AtzekoPlanoBerria(char AtzekoPlanoa[])
-{
-
-	SDL_Surface* surface;
-	SDL_Texture* texture;
-	SDL_Event event;
-
-
-	//irudia kargatu
-	surface = SDL_LoadBMP(AtzekoPlanoa);
-	if (!surface) {
-		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Ezin da argazkitik azalera sortu: %s\n", SDL_GetError());
-		return 1;
-	}
-	texture = SDL_CreateTextureFromSurface(renderer, surface);
-	if (!texture) {
-		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Ezin da azaleratik textura sortu: %s\n", SDL_GetError());
-		return 1;
-	}
-
-	SDL_FreeSurface(surface);		//Aurrekoa garbitzeko
-	SDL_PollEvent(&event);
-	SDL_RenderClear(renderer);
-	SDL_RenderCopy(renderer, texture, NULL, NULL);
 	SDL_RenderPresent(renderer);
 
 
