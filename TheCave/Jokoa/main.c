b63b2650273a8fdbee9b781050f3289a8e717aef
inakiAramburu <inaki.aramburur@alumni.mondragon.edu>
2019-12-19 09:47:34 +0100

    mer main.c iñaki

--- TheCave/Jokoa/main.c
+++ TheCave/Jokoa/main.c
@@ -1,21 +1,28 @@
 #include "Funtzioak.h"
 #include <stdio.h>
-//dev
+//inaki
 
 int main(int argc, char* str[]) {
-	char Atzekoplanoa[256] = ".\\img\\Menu.bmp", Atzekoplanoa2[128] = ".\\img\\Nivel2.bmp";		//Argazkiaren helbidea
-	if (LeihoaHasi() == 1)
-	{
-		printf("Ezin izan da 1280x720- ko bideoa ezarri: %s\n", SDL_GetError());
-		return 1;
-	}
-	
+	char Atzekoplanoa[256] = ".\\img\\Menu.bmp";		//Argazkiaren helbidea
+	char pergamino[256] = ".\\img\\pergamino.bmp";	//foto Argazkiaren helbidea
+	char Jolastu[256] = ".\\img\\Jolastu.bmp";
+	char Kontrolak[256] = ".\\img\\Kontrolak.bmp";
+	char Kredituak[256] = ".\\img\\Kredituak.bmp";
 
-	if (AtzekoPlanoBerria(Atzekoplanoa) == 1)
-	{
-		fprintf(stderr, "Ezin izan da atzeko palnoa kargatu: %s\n", SDL_GetError());
-		return 1;
-	}
+	LeihoaHasi();
+
 	SDL_Delay(2000);		//Txuriz 2 segundu
+	AtzekoPlanoBerria(Atzekoplanoa);
+	Argazkia_Sartu(pergamino, 380, 100, 480, 500);
+	Argazkia_Sartu(Jolastu, 500, 210, 270, 65);
+	Argazkia_Sartu(Kontrolak, 480, 300, 300, 72);
+	Argazkia_Sartu(Kredituak, 480, 400, 300, 72);
+	SDL_Delay(10000);
+
+
+
+
+
+
 	return 0;
 }
