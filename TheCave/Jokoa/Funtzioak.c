0d8a3fcf4c2698ec51a0a2617b346e1bdb139fc3
inakiAramburu <inaki.aramburur@alumni.mondragon.edu>
2019-12-19 10:11:30 +0100

    funtzioak.c musica

--- TheCave/Jokoa/Funtzioak.c
+++ TheCave/Jokoa/Funtzioak.c
@@ -99,3 +99,43 @@ int AtzekoPlanoBerria(char AtzekoPlanoa[])
 	return 0;
 
 }
+
+//musika funtzioa
+/*a medias*/
+void Musika(char Fitxategia[])
+{
+
+
+	// explicacion
+	//https://gigi.nullneuron.net/gigilabs/playing-a-wav-file-using-sdl2/
+
+	//inicia la parte de audio
+	SDL_Init(SDL_INIT_AUDIO);
+
+	SDL_AudioSpec wavSpec;
+	Uint32 wavLength;
+	Uint8* wavBuffer;
+
+	/*  fichategia cargatu */
+	if (SDL_LoadWAV("UnderTale.wav", &wavSpec, &wavBuffer, &wavLength) == NULL) {
+		fprintf(stderr, "no encuentra test.wav: %s\n", SDL_GetError());
+		exit(-1);
+	}
+	// abrir dependencias de audio
+
+	SDL_AudioDeviceID deviceId = SDL_OpenAudioDevice(NULL, 0, &wavSpec, NULL, 0);
+
+	// iniciar el sonido
+
+	int success = SDL_QueueAudio(deviceId, wavBuffer, wavLength);
+	SDL_PauseAudioDevice(deviceId, 0);
+
+
+
+
+	// dena itxi
+
+	//SDL_CloseAudioDevice(deviceId);
+	//SDL_FreeWAV(wavBuffer);
+	//SDL_Quit();
+}
