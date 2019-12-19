cef851c82b80e41c2eff6822566a283c5c073cf7
gorkalertxundi <gorka.lertxundi@alumni.mondragon.edu>
2019-12-19 12:18:26 +0100

    newfuntzioak

--- TheCave/Jokoa/Funtzioak.c
+++ TheCave/Jokoa/Funtzioak.c
@@ -20,14 +20,13 @@ int LeihoaHasi()
 		PANTAILA_ALTUERA,                 // Sortzeko leihoaren altuera
 		SDL_WINDOW_OPENGL                 // Erabilitako teknologia grafikoa
 	);
-
-	if (window == NULL) { 		// Leihoa komprobatzen du.
+	// Leihoa komprobachen du.
+	if (window == NULL) {
 
 		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Ezin izan da leihoa sortu: %s\n", SDL_GetError());
 		return 1;
 	}
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-	return 0;
 }
 
 //Fondoa jartzen du.
@@ -37,6 +36,7 @@ int AtzekoPlanoBerria(char AtzekoPlanoa[])
 	SDL_Surface* surface;
 	SDL_Texture* texture;
 	SDL_Event event;
+
 
 	//irudia kargatu
 	surface = SDL_LoadBMP(AtzekoPlanoa);
@@ -51,13 +51,85 @@ int AtzekoPlanoBerria(char AtzekoPlanoa[])
 	}
 
 	SDL_FreeSurface(surface);		//Aurrekoa garbitzeko
-
 	SDL_PollEvent(&event);
 	SDL_RenderClear(renderer);
 	SDL_RenderCopy(renderer, texture, NULL, NULL);
 	SDL_RenderPresent(renderer);
 
 	return 0;
+}
+
+//Argazkia jartzen du.
+int Argazkia_Sartu(char AtzekoPlanoa[], int Posx, int Posy, int luzeera, int altuera)
+{
+	SDL_Surface* surface;
+	SDL_Texture* texture;
+	SDL_Event event;
+
+
+	//irudia kargatu
+	surface = SDL_LoadBMP(AtzekoPlanoa);
+	if (!surface) {
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Ezin da argazkitik azalera sortu: %s\n", SDL_GetError());
+		return 1;
+	}
+	texture = SDL_CreateTextureFromSurface(renderer, surface);
+	if (!texture) {
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Ezin da azaleratik textura sortu: %s\n", SDL_GetError());
+		return 1;
+	}
+	//irudiaren pozizioa jarri
+
+	SDL_Rect neurriak;
+
+	neurriak.x = Posx;
+	neurriak.y = Posy;
+	neurriak.w = luzeera;
+	neurriak.h = altuera;
+
+	SDL_FreeSurface(surface);		//Aurrekoa garbitzeko
+	SDL_PollEvent(&event);
+	SDL_RenderCopy(renderer, texture, NULL, &neurriak);
+	SDL_RenderPresent(renderer);
+
+	return 0;
 
 }
 
+//musika funtzioa
+/*a medias*/
+void MusikaJarri(char Fitxategia[])
+{
+	// explicacion
+	//https://gigi.nullneuron.net/gigilabs/playing-a-wav-file-using-sdl2/
+
+	//inicia la parte de audio
+	SDL_Init(SDL_INIT_AUDIO);
+
+	SDL_AudioSpec wavSpec;
+	Uint32 wavLength;
+	Uint8* wavBuffer;
+
+	/*  fichategia cargatu */
+	if (SDL_LoadWAV(Fitxategia, &wavSpec, &wavBuffer, &wavLength) == NULL) {
+		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "no encuentra test.wav: %s\n", SDL_GetError());
+		exit(-1);
+	}
+	// abrir dependencias de audio
+
+	SDL_AudioDeviceID deviceId = SDL_OpenAudioDevice(NULL, 0, &wavSpec, NULL, 0);
+
+	// iniciar el sonido
+
+	int success = SDL_QueueAudio(deviceId, wavBuffer, wavLength);
+	SDL_PauseAudioDevice(deviceId, 0);
+
+
+
+
+	// dena itxi
+
+	//SDL_CloseAudioDevice(deviceId);
+	//SDL_FreeWAV(wavBuffer);
+	//SDL_Quit();
+}
