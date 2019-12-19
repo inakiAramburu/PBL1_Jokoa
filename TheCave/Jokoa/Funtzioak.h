447ff05bc471de4f3b373fb803f1d1b94f732898
inakiAramburu <inaki.aramburur@alumni.mondragon.edu>
2019-12-19 09:48:26 +0100

    mer funtzioa.h inaki

--- TheCave/Jokoa/Funtzioak.h
+++ TheCave/Jokoa/Funtzioak.h
@@ -6,5 +6,8 @@
 #define PANTAILA_ZABALERA 1280
 
 int LeihoaHasi();
-int AtzekoPlanoBerria(char AtzekoPlano[]);
+int Argazkia_Sartu(char AtzekoPlanoa[], int Posx, int Posy, int luzeera, int altuera);
+int AtzekoPlanoBerria(char AtzekoPlanoa[]);
+
+
 #endif
