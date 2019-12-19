8eac68f78563a14494f4e9a977e662d3236d846e
Mikel Uribesalgo Arbulu <mikel.uribesalgo@alumni.mondragon.edu>
2019-12-19 08:02:58 +0100

    Rama mikel 7

--- TheCave/Jokoa/Funtzioak.h
+++ TheCave/Jokoa/Funtzioak.h
@@ -5,6 +5,6 @@
 #define PANTAILA_ALTUERA 720
 #define PANTAILA_ZABALERA 1280
 
-int LeihoaHasi();
-int AtzekoPlanoBerria(char AtzekoPlano[]);
+void LeihoaHasi();
+void AtzekoPlanoBerria(char AtzekoPlano[]);
 #endif
