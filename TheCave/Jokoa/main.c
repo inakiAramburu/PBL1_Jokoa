76c017ca8f99a1be01a8d3671e8f27e45239573f
inakiAramburu <inaki.aramburur@alumni.mondragon.edu>
2019-12-20 17:42:58 +0100

    mando izquierda derecha

--- TheCave/Jokoa/main.c
+++ TheCave/Jokoa/main.c
@@ -4,10 +4,6 @@
 
 int main(int argc, char* str[]) {
 	char Atzekoplanoa[128] = ".\\media\\fondos\\Menu.bmp";		//Argazkiaren helbidea
-	char pergamino[128] = ".\\media\\menu\\pergamino.bmp";	//foto Argazkiaren helbidea
-	char Jolastu[128] = ".\\media\\menu\\Jolastu.bmp";
-	char Kontrolak[128] = ".\\media\\menu\\Kontrolak.bmp";
-	char Kredituak[128] = ".\\media\\menu\\Kredituak.bmp";
 	char Pertsonaia[128] = ".\\media\\player\\Player\\run\\run1.bmp";
 	//char Musika2[128] = ".\\media\\sound\\test.wav";
 	//MusikaJarri(Musika2);
@@ -16,12 +12,49 @@ int main(int argc, char* str[]) {
 
 	SDL_Delay(2000);		//Txuriz 2 segundu
 	AtzekoPlanoBerria(Atzekoplanoa);
-	Argazkia_Sartu(pergamino, 380, 100, 480, 500);
-	Argazkia_Sartu(Jolastu, 500, 210, 270, 65);
-	Argazkia_Sartu(Kontrolak, 480, 300, 300, 72);
-	Argazkia_Sartu(Kredituak, 480, 400, 300, 72);
-	Argazkia_Sartu(Pertsonaia, -10, 532, 150, 100);
 
+	Argazkia_Sartu(Pertsonaia, -10, 532, 150, 100);
+	////////////////////////////////////MANDO//////////////////////////////////////////////
+	SDL_Init(SDL_INIT_JOYSTICK);
+	SDL_Joystick* joystick = SDL_JoystickOpen(0);
+	printf("Controler name: %s\n", SDL_JoystickName(joystick));
+
+	printf("Num Axes: %d\n", SDL_JoystickNumAxes(joystick));
+
+	printf("Num Buttos: %d\n", SDL_JoystickNumButtons(joystick));
+
+
+	SDL_Event event;
+	while (1)
+	{
+		while (SDL_PollEvent(&event))
+		{
+
+			if (event.type == SDL_JOYAXISMOTION)
+			{
+				if (event.jaxis.axis == 0)
+				{
+
+					if (event.jaxis.value > 0)
+					{
+						printf("derecha\n");
+					}
+
+					if (event.jaxis.value < 0)
+					{
+						printf("izquierda\n");
+					}
+					printf("%d:\n", event.jaxis.value);
+				}
+			}
+
+		}
+
+	}
+
+
+
+	//////////////////////////////////////////////////////////////////////////////////
 	SDL_Delay(10000);
 	return 0;
 }
