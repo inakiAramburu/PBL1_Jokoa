da7d1cf96aa1d6d2479f482a5dddb87b61f343be
9ekaitz <ekaitz.casado@alumni.mondragon.edu>
2019-12-22 13:55:45 +0100

    cambios imagen

--- TheCave/Jokoa/Funtzioak.c
+++ TheCave/Jokoa/Funtzioak.c
@@ -1,10 +1,10 @@
 #include "Funtzioak.h"
 #include <SDL.h>
 
-SDL_Window* window;
-SDL_Renderer* renderer;
+SDL_Renderer* render;
+SDL_Window* leihoa;
 
-int LeihoaHasi()
+int LeihoaEtaRenderHasi()
 {
             // Initialize SDL2
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -12,7 +12,7 @@ int LeihoaHasi()
 		return 1;
 	}
 	// Create an application window with the following settings:
-	window = SDL_CreateWindow(
+	leihoa = SDL_CreateWindow(
 		"The Cave",                  			// Izenburua
 		SDL_WINDOWPOS_CENTERED,           // Leihoa erdian jarri
 		SDL_WINDOWPOS_CENTERED,           // Leihoa erdian jarri
@@ -21,12 +21,12 @@ int LeihoaHasi()
 		SDL_WINDOW_OPENGL                 // Erabilitako teknologia grafikoa
 	);
 
-	if (window == NULL) { 		// Leihoa komprobatzen du.
+	if (leihoa == NULL) { 		// Leihoa komprobatzen du.
 
 		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Ezin izan da leihoa sortu: %s\n", SDL_GetError());
 		return 1;
 	}
-	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	render = SDL_CreateRenderer(leihoa, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 	return 0;
 }
 
@@ -44,7 +44,7 @@ int AtzekoPlanoBerria(char AtzekoPlanoa[])
 		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Ezin da argazkitik azalera sortu: %s\n", SDL_GetError());
 		return 1;
 	}
-	texture = SDL_CreateTextureFromSurface(renderer, surface);
+	texture = SDL_CreateTextureFromSurface(render, surface);
 	if (!texture) {
 		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Ezin da azaleratik textura sortu: %s\n", SDL_GetError());
 		return 1;
@@ -52,11 +52,18 @@ int AtzekoPlanoBerria(char AtzekoPlanoa[])
 
 	SDL_FreeSurface(surface);		//Aurrekoa garbitzeko
 
-	SDL_PollEvent(&event);
-	SDL_RenderClear(renderer);
-	SDL_RenderCopy(renderer, texture, NULL, NULL);
-	SDL_RenderPresent(renderer);
+	SDL_RenderClear(render);
+	SDL_RenderCopy(render, texture, NULL, NULL);
+	SDL_RenderPresent(render);
 	
 	return 0;
 
+}
+
+void Amaitu()
+{
+	SDL_DestroyWindow(leihoa);
+	SDL_DestroyRenderer(render);
+
+	return;
 }
