ae9b803c1aaa710a6d8ed57fba8343a351c9d558
inakiAramburu <inaki.aramburur@alumni.mondragon.edu>
2019-12-27 12:47:44 +0100

    cambiando

--- TheCave/Jokoa/main.c
+++ TheCave/Jokoa/main.c
@@ -1,21 +1,22 @@
 #include "Funtzioak.h"
 #include <stdio.h>
-//dev
+//inaki
 
 int main(int argc, char* str[]) {
 	char Atzekoplanoa[128] = ".\\media\\fondos\\Menu.bmp";		//Argazkiaren helbidea
-	
-	char Pertsonaia[128] = ".\\media\\player\\Player\\run\\run1.bmp";
+	char Irudia[128] = ".\\media\\player\\Run.bmp";	//foto Argazkiaren 
+	int i;
 	//char Musika2[128] = ".\\media\\sound\\test.wav";
 	//MusikaJarri(Musika2);
-	
 	LeihoaHasi();
 
 	SDL_Delay(2000);		//Txuriz 2 segundu
-	AtzekoPlanoBerria(Atzekoplanoa);
 	
-	Argazkia_Sartu(Pertsonaia, -10, 532, 150, 100);
-	teklatua();
+			AtzekoPlanoBerria(Atzekoplanoa);
+			Argazkia_Sartu(Irudia, (128 * i), 0);
+			SDL_Delay(90);
+
+
 	SDL_Delay(10000);
 	return 0;
 }
