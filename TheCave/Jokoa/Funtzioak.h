e8cbcee7c745af6e74cb07487701d9132dd75d6b
9ekaitz <ekaitz.casado@alumni.mondragon.edu>
2019-12-27 15:08:44 +0100

    Leihoa ixteko

--- TheCave/Jokoa/Funtzioak.h
+++ TheCave/Jokoa/Funtzioak.h
@@ -6,12 +6,16 @@
 #define PANTAILA_ZABALERA 1280
 
 typedef enum {MENUA, LEHEN, BIGARREN}PANTAILAK;
+typedef enum {IDLE, KORRIKA, SALTO, ERASO, HIL }SPRITE;
+typedef enum{HILDA, BIZIRIK}EGOERA;
+typedef enum{EZJOKATZEN, JOKATZEN}JOKOA;
 
 int LeihoaEtaRenderHasi();
 void KargatuIrudiak(PANTAILAK Pantaila);
 void ImgKargatu(char src[]);
 void RenderPrestatu();
 void Irudikatu();
+void EbentuakKonprobatu();
 
 void Amaitu();
 #endif
--- TheCave/Jokoa/main.c
+++ TheCave/Jokoa/main.c
@@ -5,26 +5,42 @@
 int main(int argc, char* str[]) {
 
 	PANTAILAK Pantaila;		//Zein pantailan dagoen jokalaria
+	JOKOA Jokoa = EZJOKATZEN;
 
 	int KargaMenua = 0;		//Argazkiak bakarrik behin kargatzeko pantaila bakoitzean
 	int KargaLehen = 0;		//""
 	int KargaBigarren = 0;		//""
-
-	Pantaila = MENUA;		//Hasieran Menu pantailan dago jokalaria
 
 	if (LeihoaEtaRenderHasi() == 1)		//Lehioa eta renderizatua hasieratzen du
 	{
 		printf("Ezin izan da 1280x720- ko bideoa ezarri: %s\n", SDL_GetError());		//Erroreak
 		return 1;
 	}
-	
-	if (!KargaMenua)
+
+	Pantaila = MENUA;		//Hasieran Menu pantailan dago jokalaria
+	Jokoa = JOKATZEN;
+
+	while (Jokoa)
 	{
-		KargatuIrudiak(Pantaila);
+		while (Pantaila == MENUA)
+		{
+			if (!KargaMenua)
+			{
+				KargatuIrudiak(Pantaila);
+				KargaMenua = 1;
+			}
+			RenderPrestatu();
+			Irudikatu();
+			SDL_Delay(50);
+			EbentuakKonprobatu();
+		}
+		while (Pantaila == LEHEN)
+		{
+
+		}
 	}
 
-	RenderPrestatu();
-	Irudikatu();
+	
 	SDL_Delay(2000);		//Txuriz 2 segundu
 	Amaitu();
 	return 0;
